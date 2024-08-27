b7685c49eb412f96b8afcc426439b6e6f2e74a4c
IK-github-97 <indranilkundu97@outlook.com>
2024-08-27 19:40:34 +0530

    Update Azure Key Vault

--- Program.cs
+++ Program.cs
@@ -10,16 +10,22 @@ namespace EmployeeDetails
     {
         public static void Main(string[] args)
         {
-            var keyVaultName = "ik-key-vault-store";
-            var secretName = "EmployeesDbConnection";
-
-            var client = new SecretClient(new Uri($"https://{keyVaultName}.vault.azure.net/"), new DefaultAzureCredential());
-            var EmployeeDetailsContext = client.GetSecret(secretName).Value.Value; //secret value
-
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<EmployeeDetailsContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString(EmployeeDetailsContext) ?? throw new InvalidOperationException("Connection string 'EmployeeDetailsContext' not found.")));
+            /*builder.Services.AddDbContext<EmployeeDetailsContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDetailsContext") ?? throw new InvalidOperationException("Connection string 'EmployeeDetailsContext' not found.")));
+*/
+            // Retrieve the secret from Azure Key Vault
+            var keyVaultName = "ik-key-vault-store"; // Replace with your actual Key Vault name
+            var secretName = "EmployeesDbConnection"; // Replace with your secret name
+            var client = new SecretClient(new Uri($"https://{keyVaultName}.vault.azure.net/"), new DefaultAzureCredential());
+            var secretValue = client.GetSecret(secretName).Value.Value;
 
+            // Use secretValue as your connection string
+            //var connectionString = string.Format(builder.Configuration.GetConnectionString("EmployeeDetailsContext"), secretValue);
+            var connectionString = secretValue;
+
+            builder.Services.AddDbContext<EmployeeDetailsContext>(options =>
+    options.UseSqlServer(connectionString));
             // Add services to the container.
 
             builder.Services.AddControllers();
